3ae013326a4cdf53b39a8fe97df52e17f7272d35
CrestLi <crest@selefra.io>
2023-03-01 18:20:25 +0800

    fix: registry

--- .github/scripts/registry.sh
+++ .github/scripts/registry.sh
@@ -23,20 +23,20 @@ if [ -d "provider/consul/$version" ];then rm -rf provider/consul/$version ; else
 
 for f in $FOR; do
   echo "$f"
-  darwin_arm64=`cat selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME* | grep darwin_arm64 | awk -F ' ' '{print $1}'`
-  darwin_amd64=`cat selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME* | grep darwin_amd64 | awk -F ' ' '{print $1}'`
-  windows_amd64=`cat selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME* | grep windows_amd64 | awk -F ' ' '{print $1}'`
-  linux_amd64=`cat selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME* | grep linux_amd64 | awk -F ' ' '{print $1}'`
-  linux_arm64=`cat selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME* | grep linux_arm64 | awk -F ' ' '{print $1}'`
-  windows_arm64=`cat selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME* | grep windows_arm64 | awk -F ' ' '{print $1}'`
-  sed "${sedi[@]}" "s#{{.PackageName}}#selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.Source}}#https://github.com/selefra/selefra-provider-REPLACE-ME-TO-YOUR-PROVIDER-NAME#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.CheckSumLinuxARM64}}#${linux_arm64}#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.CheckSumLinuxAMD64}}#${linux_amd64}#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.CheckSumWindowsARM64}}#${windows_arm64}#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.CheckSumWindowsAMD64}}#${windows_amd64}#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.CheckSumDarwinARM64}}#${darwin_arm64}#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
-  sed "${sedi[@]}" "s#{{.CheckSumDarwinAMD64}}#${darwin_amd64}#g" provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/$version/supplement.yaml
+  darwin_arm64=`cat selefra-provider-consul* | grep darwin_arm64 | awk -F ' ' '{print $1}'`
+  darwin_amd64=`cat selefra-provider-consul* | grep darwin_amd64 | awk -F ' ' '{print $1}'`
+  windows_amd64=`cat selefra-provider-consul* | grep windows_amd64 | awk -F ' ' '{print $1}'`
+  linux_amd64=`cat selefra-provider-consul* | grep linux_amd64 | awk -F ' ' '{print $1}'`
+  linux_arm64=`cat selefra-provider-consul* | grep linux_arm64 | awk -F ' ' '{print $1}'`
+  windows_arm64=`cat selefra-provider-consul* | grep windows_arm64 | awk -F ' ' '{print $1}'`
+  sed "${sedi[@]}" "s#{{.PackageName}}#selefra-provider-consul#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.Source}}#https://github.com/selefra/selefra-provider-consul#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.CheckSumLinuxARM64}}#${linux_arm64}#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.CheckSumLinuxAMD64}}#${linux_amd64}#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.CheckSumWindowsARM64}}#${windows_arm64}#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.CheckSumWindowsAMD64}}#${windows_amd64}#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.CheckSumDarwinARM64}}#${darwin_arm64}#g" provider/consul/$version/supplement.yaml
+  sed "${sedi[@]}" "s#{{.CheckSumDarwinAMD64}}#${darwin_amd64}#g" provider/consul/$version/supplement.yaml
 done
 
 if [[ "$VERSION" != "$version" ]]; then
@@ -51,6 +51,6 @@ if [[ "$VERSION" != "$version" ]]; then
     sed -n '/^ /p' provider/consul/metadata.yaml >> provider/template/metadata.yaml
   fi
   echo "  - ${version}" >> provider/template/metadata.yaml
-  cat provider/template/metadata.yaml > provider/REPLACE-ME-TO-YOUR-PROVIDER-NAME/metadata.yaml
+  cat provider/template/metadata.yaml > provider/consul/metadata.yaml
   mv provider/template/metadata.yaml.bak provider/template/metadata.yaml
 fi
